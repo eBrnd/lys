a7d3a71246d63c0411a8c69073678eca463aba55
eBrnd <github@ebrnd.de>
2015-02-26 00:18:12 +0100

    move common code out of switch case.

--- software/color.c
+++ software/color.c
@@ -3,53 +3,47 @@
 // HSV to RGB color space conversion.
 // We use a HSV range from 0 .. 1536 so every (3*8bit) RGB value can be expressed as a HSV value.
 void color_hsv2rgb(struct color_hsv* in, struct color_rgb* out) {
-  uint8_t col = (in->v * in->s) / 255;
-  uint8_t min = in->v - col;
+  uint8_t col = (in->v * in->s) / 255; // Span between darkest and brightest channel.
+  uint8_t min = in->v - col; // Minimum (brightness of the darkest channel).
+  uint8_t ramp = in->h >> 8; // Ramp of the HSV diagram we're on.
+  uint8_t hue = in->h % 256; // Position on hue ramp.
 
-  // Find out which ramp of the HSV diagram we're on.
-  if (in->h < 256) {         // Green up.
-    uint8_t hue = in->h;
-    hue = hue * col / 255;
+  if (ramp % 2) // We're on a downward ramp.
+    hue = ~hue;
 
+  hue = hue * col / 255; // Scale position on ramp to slope.
+  hue += min;
+
+  switch (ramp) {
+  case 0:  // Green up.
     out->r = in->v;
-    out->g = min + hue;
+    out->g = hue;
     out->b = min;
-  } else if (in->h < 512) {  // Red down.
-    uint8_t hue = in->h - 256;
-    hue = (255 - hue) * col / 255;
-
-    out->r = min + hue;
+    break;
+  case 1:  // Red down.
+    out->r = hue;
     out->g = in->v;
     out->b = min;
-  } else if (in->h < 768) {  // Blue up.
-    uint8_t hue = in->h - 512;
-    hue = hue * col / 255;
-
+    break;
+  case 2:  // Blue up.
     out->r = min;
     out->g = in->v;
-    out->b = min + hue;
-  } else if (in->h < 1024) { // Green down.
-    uint8_t hue = in->h - 768;
-    hue = (255 - hue) * col / 255;
-
+    out->b = hue;
+    break;
+  case 3:  // Green down.
     out->r = min;
-    out->g = min + hue;
+    out->g = hue;
     out->b = in->v;
-  } else if (in->h < 1280) { // Red up.
-    uint8_t hue = in->h - 1024;
-    hue = hue * col / 255;
-
-    out->r = min + hue;
+    break;
+  case 4:  // Red up.
+    out->r = hue;
     out->g = min;
     out->b = in->v;
-  } else if (in->h < 1536) { // Blue down.
-    uint8_t hue = in->h -1280;
-    hue = (255 - hue) * col / 255;
-
-    out->b = min + hue;
-    out->g = min;
+    break;
+  case 5:  // Blue down.
     out->r = in->v;
-  } else {
-    // Fail.
+    out->g = min;
+    out->b = hue;
+    break;
   }
 }
