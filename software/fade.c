391cb6f5e4c74eac094dbcac537bcb0d12ff13f1
eBrnd <github@ebrnd.de>
2015-03-23 22:02:40 +0100

    fade interpolation in int

--- software/fade.c
+++ software/fade.c
@@ -7,7 +7,7 @@
 
 struct color_hsv fade_target = { 0, 0, 255 };
 struct color_hsv fade_start = { 0, 0, 255 };
-unsigned fade_steps_left = 0;
+unsigned fade_steps = 0;
 unsigned fade_duration = 1;
 
 // Setup: Register timer.
@@ -19,21 +19,21 @@ void fade_setup() {
 void fade_set_target(struct color_hsv* new_target, unsigned duration) {
 	pwm_get_hsv(&fade_start);
 	memcpy(&fade_target, new_target, sizeof(fade_target));
+
 	if (duration == 0) duration = 1;
-	fade_duration = fade_steps_left = duration;
+	fade_duration = duration;
+	fade_steps = 0;
 }
 
 // This should be called in more or less regular intervals and it does the actual fading.
 void fade_step() {
-	if(fade_steps_left) {
-		fade_steps_left--;
-
-		float fade_delta_t = 1.f - ((float)fade_steps_left / (float)fade_duration);
+	if(fade_steps < fade_duration) {
+		fade_steps++;
 
 		struct color_hsv next_color = {
-			(float)fade_start.h + fade_delta_t * ((float)fade_target.h - (float)fade_start.h),
-			(float)fade_start.s + fade_delta_t * ((float)fade_target.s - (float)fade_start.s),
-			(float)fade_start.v + fade_delta_t * ((float)fade_target.v - (float)fade_start.v)
+			(int32_t)fade_start.h + (fade_steps * ((int32_t)fade_target.h - (int32_t)fade_start.h) / fade_duration),
+			(int32_t)fade_start.s + (fade_steps * ((int32_t)fade_target.s - (int32_t)fade_start.s) / fade_duration),
+			(int32_t)fade_start.v + (fade_steps * ((int32_t)fade_target.v - (int32_t)fade_start.v) / fade_duration)
 		};
 
 		pwm_set_hsv(&next_color);
