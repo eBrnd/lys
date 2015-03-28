91b4ef39ed2d8b1c22d8052f02f2e18a2a81dda9
eBrnd <github@ebrnd.de>
2015-03-28 21:50:13 +0100

    remove logarithmic color conversion

--- software/pwm.c
+++ software/pwm.c
@@ -24,21 +24,7 @@ struct color_hsv current_color = {0, 0, 0};
 // Forward declarations.
 void setup_port();
 void setup_timer();
-
-// Logarithm table for smoother visual appearance.
-const uint16_t pwmtable[] PROGMEM = {
-  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-  3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6,
-  6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 13, 13,
-  13, 14, 14, 15, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
-  26, 26, 27, 28, 29, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 47, 48,
-  49, 51, 52, 53, 55, 56, 58, 60, 61, 63, 65, 66, 68, 70, 72, 74, 76, 78, 80, 82, 85, 87, 89, 92,
-  94, 97, 100, 102, 105, 108, 111, 114, 117, 121, 124, 127, 131, 134, 138, 142, 146, 150, 154, 158,
-  162, 167, 171, 176, 181, 186, 191, 196, 202, 207, 213, 219, 225, 231, 237, 244, 250, 257, 264,
-  271, 279, 287, 294, 303, 311, 319, 328, 337, 346, 356, 366, 376, 386, 397, 407, 419, 430, 442,
-  454, 467, 479, 493, 506, 520, 534, 549, 564, 579, 595, 612, 628, 646, 663, 682, 700, 719, 739,
-  760, 780, 802, 824, 846, 870, 893, 918, 943, 969, 996, 1023
-};
+void set_rgb(struct color_rgb* cr);
 
 // Setup - call this at boot time.
 void pwm_setup() {
@@ -55,26 +41,19 @@ void pwm_set_hsv(struct color_hsv* ch) {
 
   color_hsv2rgb(ch, &cr);
 
-  pwm_set_rgb(&cr);
+  set_rgb(&cr);
 }
 
-void pwm_set_rgb(struct color_rgb* cr) {
-  r = pgm_read_word(&pwmtable[cr->r]);
-  g = pgm_read_word(&pwmtable[cr->g]);
-  b = pgm_read_word(&pwmtable[cr->b]);
+void set_rgb(struct color_rgb* cr) {
+  r = cr->r * 4;
+  g = cr->g * 4;
+  b = cr->b * 4;
 }
 
 //Get current color
 void pwm_get_hsv(struct color_hsv* ch) {
   memcpy(ch, &current_color, sizeof(struct color_hsv));
 }
-
-void pwm_get_rgb(struct color_rgb* cr) {
-  cr->r = r;
-  cr->g = g;
-  cr->b = b;
-}
-
 
 // This is run from the timer ISR.
 static inline void do_pwm() {
