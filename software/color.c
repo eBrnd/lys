62bccb8d8457dd8e8fd8a52c11e61e110f19fbef
eBrnd <github@ebrnd.de>
2015-03-29 00:15:23 +0100

    do hsv to rgb conversion in 10bit (so we can do the brightness correction on the v channel only)

--- software/color.c
+++ software/color.c
@@ -3,16 +3,22 @@
 // HSV to RGB color space conversion.
 // We use a HSV range from 0 .. 1536 so every (3*8bit) RGB value can be expressed as a HSV value.
 void color_hsv2rgb(struct color_hsv* in, struct color_rgb* out) {
-  uint8_t col = (uint16_t)(in->v * in->s) / 255; // Span between darkest and brightest channel.
-  uint8_t min = in->v - col; // Minimum (brightness of the darkest channel).
+
+  uint16_t v = 4 * in->v;
+
+  uint16_t col = ((uint32_t)v * in->s) / 255UL; // Span between darkest and brightest channel.
+  uint16_t min = v - col; // Minimum (brightness of the darkest channel).
   uint8_t ramp = in->h >> 8; // Ramp of the HSV diagram we're on.
-  uint8_t hue = in->h % 256; // Position on hue ramp.
+  uint16_t hue = in->h % 256; // Position on hue ramp.
 
   if (ramp % 2) // We're on a downward ramp.
-    hue = ~hue;
+    hue = 255 - hue;
 
-  hue = hue * col / 255; // Scale position on ramp to slope.
+  hue = ((uint32_t)hue * col) / 255UL; // Scale position on ramp to slope.
   hue += min;
+
+  min /= 4;
+  hue /= 4;
 
   switch (ramp) {
   case 0:  // Green up.
